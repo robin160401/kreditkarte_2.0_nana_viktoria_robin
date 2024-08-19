330135672ff2594eda05d8fe7bdc58258944d1dd
Nana Leven <nana.leven@mail.de>
2024-08-19 17:20:33 +0200

    doch noch nicht ganz fertig

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import CardDetails from "./classes/CardDetails";
 
 const cardNumber = document.getElementById("card-number") as HTMLInputElement;
 const cardHolder = document.getElementById("card-holder") as HTMLInputElement;
-const expirationDate = document.getElementById("expiration-date") as HTMLInputElement;
+const expirationDate = document.getElementById(
+  "expiration-date"
+) as HTMLInputElement;
 const cvv = document.getElementById("cvv") as HTMLInputElement;
 const form = document.getElementById("credit-form") as HTMLFormElement;
 const digitsH2tag = document.getElementById("digits") as HTMLHeadElement;
 const fullNameH2Tag = document.getElementById("fullName") as HTMLHeadElement;
 const dateH2Tag = document.getElementById("date") as HTMLHeadElement;
+const mainContainer = document.getElementById("container") as HTMLDivElement;
 
 // function isValidDate(dateString: string): boolean {
 //   const date = new Date(dateString);
@@ -18,55 +21,65 @@ const arrayOfCreditCards: CardDetails[] = [];
 
 console.log(expirationDate.value);
 
-function validateInfos(event: Event){
+function validateInfos(event: Event) {
   event.preventDefault();
   const cardNumberString = cardNumber.value;
   const cvvString = cvv.value;
-  
-  if (cardNumberString.length !== 16){
-    window.alert("Card Number is Invalid")
-  } else if (cvvString.length !== 3){
-    window.alert("CVV is Invalid")
-  } else if (arrayOfCreditCards.length >= 5){
-    window.alert("Maximum Capacity is 5 Creditcards")
+
+  if (cardNumberString.length !== 16) {
+    window.alert("Card Number is Invalid");
+  } else if (cvvString.length !== 3) {
+    window.alert("CVV is Invalid");
+  } else if (arrayOfCreditCards.length >= 5) {
+    window.alert("Maximum Capacity is 5 Creditcards");
   } else {
     changeHTMLCreditcardsvalues();
     createCreditcard();
+    cardNumber.value = " ";
+    cardHolder.value = " ";
+    cvv.value = " ";
     createExtraCreditCard();
     console.log(arrayOfCreditCards);
   }
 }
 
-function createCreditcard(){
-  const creditcard = new CardDetails(Number(cardNumber.value), cardHolder.value, expirationDate.value, Number(cvv.value))
+function createCreditcard() {
+  const creditcard = new CardDetails(
+    Number(cardNumber.value),
+    cardHolder.value,
+    expirationDate.value,
+    Number(cvv.value)
+  );
   arrayOfCreditCards.push(creditcard);
 }
 
-function changeHTMLCreditcardsvalues(){
+function changeHTMLCreditcardsvalues() {
   const numberWithSpaces = [];
   const cardNumberString = cardNumber.value.toString();
   const cardNumberArray = cardNumberString.split("");
 
   console.log(cardNumberArray);
 
-  for (let i = 1, j = 0; i < cardNumberString.length + 1; i++, j++){
+  for (let i = 1, j = 0; i < cardNumberString.length + 1; i++, j++) {
     numberWithSpaces.push(cardNumberArray[j]);
-    if (i % 4 === 0 && i !== 0){
+    if (i % 4 === 0 && i !== 0) {
       numberWithSpaces.push(" ");
     }
-}
+  }
 
   const finalNumber = numberWithSpaces.join("");
   digitsH2tag.textContent = finalNumber;
   fullNameH2Tag.textContent = cardHolder.value;
-  dateH2Tag.textContent = expirationDate.value.replace("-", "/").replace("20", "");
+  dateH2Tag.textContent = expirationDate.value
+    .replace("-", "/")
+    .replace("20", "");
 }
 
-function createExtraCreditCard(){
+function createExtraCreditCard() {
   const cardContainer = document.querySelector(".credit-card");
   const copyCardContainer = cardContainer!.cloneNode(true);
-  document.body.appendChild(copyCardContainer); 
-}
 
+  mainContainer.appendChild(copyCardContainer);
+}
 
 form.addEventListener("submit", validateInfos);
